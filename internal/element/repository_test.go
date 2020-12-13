ff3384fb53ac2fef8f517a3ffab9c7eb5f32172b
andress134 <54506252+andress134@users.noreply.github.com>
2020-12-13 11:54:14 +0200

    Update repository_test.go

--- internal/element/repository_test.go
+++ internal/element/repository_test.go
@@ -5,11 +5,11 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/sclevine/agouti/api"
-	. "github.com/sclevine/agouti/internal/element"
-	. "github.com/sclevine/agouti/internal/matchers"
-	"github.com/sclevine/agouti/internal/mocks"
-	"github.com/sclevine/agouti/internal/target"
+	"github.com/andress134/agouti/api"
+	. "github.com/andress134/agouti/internal/element"
+	. "github.com/andress134/agouti/internal/matchers"
+	"github.com/andress134/agouti/internal/mocks"
+	"github.com/andress134/agouti/internal/target"
 )
 
 var _ = Describe("ElementRepository", func() {
