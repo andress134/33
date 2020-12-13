6227c41b890dcbec9dc2b3f7e425f623ba97fdec
andress134 <54506252+andress134@users.noreply.github.com>
2020-12-13 12:06:53 +0200

    Update webdriver_test.go

--- api/webdriver_test.go
+++ api/webdriver_test.go
@@ -9,8 +9,8 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/sclevine/agouti/api"
-	"github.com/sclevine/agouti/api/internal/mocks"
+	. "github.com/andress134/agouti/api"
+	"github.com/andress134/agouti/api/internal/mocks"
 )
 
 type roundTripperFunc func(*http.Request) (*http.Response, error)
