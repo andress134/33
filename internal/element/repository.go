4a833e0f1e9bed7a94da9c0ae5f79f86e3c0dad6
andress134 <54506252+andress134@users.noreply.github.com>
2020-12-13 11:54:38 +0200

    Update repository.go

--- internal/element/repository.go
+++ internal/element/repository.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/sclevine/agouti/api"
-	"github.com/sclevine/agouti/internal/target"
+	"github.com/andress134/agouti/api"
+	"github.com/andress134/agouti/internal/target"
 )
 
 type Repository struct {
