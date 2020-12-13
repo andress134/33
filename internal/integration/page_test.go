ec4c03202082d6baf84e16fca6cdaedda550d5ad
andress134 <54506252+andress134@users.noreply.github.com>
2020-12-13 11:56:52 +0200

    Update page_test.go

--- internal/integration/page_test.go
+++ internal/integration/page_test.go
@@ -10,8 +10,8 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/sclevine/agouti"
-	. "github.com/sclevine/agouti/matchers"
+	"github.com/andress134/agouti"
+	. "github.com/andress134/agouti/matchers"
 )
 
 func testPage(browserName string, newPage pageFunc) {
