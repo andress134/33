05264f7feed1ab38e1e4c1f6648d8315e9036d85
andress134 <54506252+andress134@users.noreply.github.com>
2020-12-13 11:47:57 +0200

    Update selection_actions.go

--- selection_actions.go
+++ selection_actions.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"github.com/sclevine/agouti/api"
-	"github.com/sclevine/agouti/internal/element"
-	"github.com/sclevine/agouti/internal/target"
+	"github.com/andress134/agouti/api"
+	"github.com/andress134/agouti/internal/element"
+	"github.com/andress134/agouti/internal/target"
 )
 
 type actionsFunc func(element.Element) error
