06491fb968e52a71a26b5e584e1e62c7dd466edd
andress134 <54506252+andress134@users.noreply.github.com>
2020-12-13 11:57:17 +0200

    Update selection_test.go

--- internal/integration/selection_test.go
+++ internal/integration/selection_test.go
@@ -3,8 +3,8 @@ package integration_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/sclevine/agouti"
-	. "github.com/sclevine/agouti/matchers"
+	"github.com/andress134/agouti"
+	. "github.com/andress134/agouti/matchers"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
