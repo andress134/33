d2526a1b740d31ecdd075c33af6cd26829dac358
andress134 <54506252+andress134@users.noreply.github.com>
2020-12-13 11:36:33 +0200

    Update selectable.go

--- selectable.go
+++ selectable.go
@@ -1,9 +1,9 @@
 package agouti
 
 import (
-	"github.com/sclevine/agouti/api"
-	"github.com/sclevine/agouti/internal/element"
-	"github.com/sclevine/agouti/internal/target"
+	"github.com/andress134/agouti/api"
+	"github.com/andress134/agouti/internal/element"
+	"github.com/andress134/agouti/internal/target"
 )
 
 type Selectors interface {
